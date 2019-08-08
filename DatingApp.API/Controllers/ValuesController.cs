73d1d8cd980a6bea920f2f6570b302a61f00f18f
infocodify <bledinjo@hotmail.com>
2019-08-08 14:01:25 -0400

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    // http://localhost:5000/api/
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
+        // GET api/values
+        [HttpGet]
+        //Async
+        public async Task<IActionResult> GetValues()
+        {
+            // throw new Exception("Text Exception");
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        // //sync
+        // public IActionResult GetValues()
+        // {
+        //     // throw new Exception("Text Exception");
+        //     var values = _context.Values.ToList();
+        //     return Ok(values);
+        // }
+
+        // GET api/values/5
+        //Select specific Value
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            //resurn the default -- Labda Expresion
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
